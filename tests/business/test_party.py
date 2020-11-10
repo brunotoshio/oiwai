5c893251bdd232a9b0eeb6c2ba48b37f96c24fb7
Bruno Toshio Sugano <brunotoshio@gmail.com>
2020-11-10 16:54:07 +0900

    create party in mongo

--- tests/business/test_party.py
+++ tests/business/test_party.py
@@ -7,15 +7,17 @@ from business import party
 class TestCreateParty:
     """ Tests for create party
     """
-    def test_success(self):
-        result, errors = party.create_party('Birthday')
+    def test_success(self, mongo):
+        result, errors = party.create_party(mongo, 'Birthday')
 
         assert result
         assert errors is None
+        assert mongo.parties.count_documents({}) == 1
 
-    def test_missing_name(self):
-        result, errors = party.create_party('')
+    def test_missing_name(self, mongo):
+        result, errors = party.create_party(mongo, '')
 
         assert not result
         assert len(errors) > 0
         assert 'name is required' in errors
+        assert mongo.parties.count_documents({}) == 0
