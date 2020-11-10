5c45740b4b526669ca8fa493ffeb24e1fd682d6e
Bruno Toshio Sugano <brunotoshio@gmail.com>
2020-11-10 13:21:45 +0900

    adding test support

---
+++ src/tests/business/test_party.py
@@ -0,0 +1,20 @@
+""" Tests for party module
+"""
+
+from business import party
+
+class TestCreateParty:
+    """ Tests for create party
+    """
+    def test_success(self):
+        result, errors = party.create_party('Birthday')
+
+        assert result
+        assert errors is None
+
+    def test_missing_name(self):
+        result, errors = party.create_party('')
+
+        assert not result
+        assert len(errors) > 0
+        assert 'name is required' in errors
